03c0ce15ef0de1fef962ab77b6960585374af950
Nikolay Stoyanov <76910101+NikolayStoyanovv@users.noreply.github.com>
2021-02-04 14:30:55 +0100

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,4 +1,7 @@
-﻿#include<stdio.h>
+/*Задача 8 Напишете функция void reverse(char s[]), която обръща низа char s[
+]. Използвайте for цикъл с два аргумента, разделени със запетая
+i = 0, j = strlen(s) – 1; i < j; i++, j-- , за да обърнете стринга.﻿*/
+#include<stdio.h>
 #include<string.h>
 void reverse(char s[]){
     for(int i=strlen(s);i>=0;i--){
